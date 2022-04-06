00cb680cc584e139799000b7aaa724a38b8442df
Taras Iskiv <taras.iskiv@gmail.com>
2022-04-06 22:36:41 +0200

    Add project files.

---
+++ DNSMXRecordsConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+Console.WriteLine("Hello, World!");
+
+var domens = new List<string>()
+{
+    "gmail.com",
+    "yahoo.com",
+    "hotmail.com",
+    "aol.com",
+    "outlook.com",
+    "icloud.com",
+    "protonmail.com",
+    "gmx.com"
+};
+
+
+Console.WriteLine(args[1]);
